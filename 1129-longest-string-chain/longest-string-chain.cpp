f38e0eb4923c641ac7a605fe90df99ce284d9269
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2026-07-10 14:49:13 +0530

    Time: 89 ms (40.80%) | Memory: 36.1 MB (34.46%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -14,32 +14,28 @@ public:
         {
             for(int j = 0 ; j<i ; j++)
             {
-                if(words[i].size() == words[j].size()+1 && lcs(words[i],words[j]) && dp[i] < dp[j]+1)
+                if(checkPossible(words[i],words[j]) && dp[i] < dp[j]+1)
                 dp[i] = dp[j]+1;
             }
         }
         return *max_element(dp.begin(),dp.end());
     }
-    bool lcs(string s1 , string s2)
+    bool checkPossible(string &s, string &t)
     {
-        int n = s1.size();
-        int m = s2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i = 0 ; i<n ; i++)
-        dp[i][0] = 0;
-        for(int i = 0 ; i<m ; i++)
-        dp[0][i] = 0;
-
-        for(int i = 1 ; i<=n ; i++)
+        if(s.size() != t.size() + 1)
+        return false;
+        int i = 0, j = 0;
+        while(i < s.size())
         {
-            for(int j = 1 ; j<=m ; j++)
+            if(j < t.size() && s[i] == t[j])
             {
-                if(s1[i-1] == s2[j-1])
-                dp[i][j] = 1+dp[i-1][j-1];
-                else
-                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                i++, j++;
             }
+            else
+            i++;
         }
-        return dp[n][m] == m;
+        if(i == s.size() && j == t.size())
+        return true;
+        return false;
     }
 };
