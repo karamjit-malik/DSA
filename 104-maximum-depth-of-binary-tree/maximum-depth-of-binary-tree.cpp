d814301a768981569c5ca1d82b9176e39e5ff880
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2026-07-13 04:04:38 +0530

    Time: 3 ms (3.77%) | Memory: 22 MB (17.10%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -15,18 +15,8 @@ public:
     {
         if(root == nullptr)
         return 0;
-        int len = 1;
-        return height(root);
-    }
-    int height(TreeNode* root)
-    {
-        if(root == nullptr)
-        return 1;
-        int left = 0 , right = 0;
-        if(root->left)
-        left = height(root->left);
-        if(root->right)
-        right = height(root->right);
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
         return 1+max(left,right);
     }
 };
