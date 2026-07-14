644e4056b64939cdc36ded739d9c658705d76915
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2026-07-15 03:46:45 +0530

    Time: 31 ms (28.07%) | Memory: 23.4 MB (92.63%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -6,25 +6,36 @@ public:
         int m = grid[0].size();
         if(grid[0][0] == 1 || grid[n-1][m-1] == 1)
         return -1;
-        vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
-        queue<pair<int,int>> q;
-        q.push({0,0});
-        grid[0][0] = 1;
-        while(!q.empty())
+        auto comp = [](pair<pair<int,int>,int> a,pair<pair<int,int>,int> b)
         {
-            int i = q.front().first;
-            int j = q.front().second;
-            q.pop();
-            int curr = grid[i][j];
-            if(i == n-1 && j == n-1)
-            return curr;
+            return a.second > b.second;
+        };
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
+        priority_queue<pair<pair<int,int>,int> , vector<pair<pair<int,int>,int>> , decltype(comp)> heap(comp);
+        dist[0][0] = 1;
+        heap.push({{0,0},1});
+        while(!heap.empty())
+        {
+            int i = heap.top().first.first;
+            int j = heap.top().first.second;
+            int dis = heap.top().second;
+            heap.pop();
+            if(dis > dist[i][j])
+            continue;
+            if(i == n-1 && j == m-1)
+            return dis;
             for(auto [dx,dy] : dir)
             {
-                int x = i+dx , y = j+dy;
-                if(x>=0 && x<n && y>=0 && y<n && grid[x][y] == 0)
+                int x = i + dx;
+                int y = j + dy;
+                if(x >= 0 && y >= 0 && x < n && y < m && grid[x][y] == 0)
                 {
-                    grid[x][y] = curr+1;
-                    q.push({x,y});
+                    if(dis + 1 < dist[x][y])
+                    {
+                        dist[x][y] = dis + 1;
+                        heap.push({{x,y}, dis + 1});
+                    }
                 }
             }
         }
