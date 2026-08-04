a3d1be17d5d1f111207fc2328175255b5c97134f
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2026-08-04 16:07:28 +0530

    Time: 1163 ms (23.60%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,1 @@
+select b.contest_id , round(((count(b.contest_id)/(SELECT COUNT(*) FROM Users))*100),2) as percentage from users a join register b on a.user_id = b.user_id group by b.contest_id order by percentage desc , b.contest_id asc
