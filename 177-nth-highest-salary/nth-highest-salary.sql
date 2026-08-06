d20149271eed8652afcbdeb8e4b80b14dbbabf4a
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2026-08-06 20:17:37 +0530

    Time: 720 ms (7.60%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,6 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+    select distinct salary from (select salary , dense_rank() over(order by salary desc) rnk from employee )a where a.rnk = N
+  );
+END
