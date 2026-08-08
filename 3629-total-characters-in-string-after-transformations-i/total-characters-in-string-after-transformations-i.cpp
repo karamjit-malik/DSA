7e457993685f585ee18ee0e82fb4ec503c7d890b
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2026-08-08 19:17:57 +0530

    Time: 837 ms (31.07%) | Memory: 467 MB (54.24%) - LeetSync

---
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lengthAfterTransformations(string s, int t)
+    {
+        const int MOD = 1e9 + 7;
+        vector<long long> freq(26, 0);
+        for(char c : s)
+        freq[c - 'a']++;
+        while(t--)
+        {
+            vector<long long> newFreq(26, 0);
+            for(int i = 0; i < 25; i++)
+            newFreq[i + 1] = (newFreq[i + 1] + freq[i]) % MOD;
+            newFreq[0] = (newFreq[0] + freq[25]) % MOD;
+            newFreq[1] = (newFreq[1] + freq[25]) % MOD;
+
+            freq = newFreq;
+        }
+        long long ans = 0;
+        for(long long x : freq)
+        ans = (ans + x) % MOD;
+        return ans;
+    }
+};
