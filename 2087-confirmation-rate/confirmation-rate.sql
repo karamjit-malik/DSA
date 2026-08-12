338e0a5715a1a5cd2f21682afed5047069831a28
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2026-08-12 20:29:34 +0530

    Time: 744 ms (42.24%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,2 +1,16 @@
-SELECT s.user_id,
-ROUND(COALESCE(SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) / COUNT(c.action),0),2) AS confirmation_rate FROM Signups s LEFT JOIN Confirmations c ON s.user_id = c.user_id GROUP BY s.user_id;
+SELECT
+    s.user_id,
+    ROUND(
+        CASE
+            WHEN COUNT(c.user_id) > 0
+            THEN
+                SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END)
+                / COUNT(c.user_id)
+            ELSE 0
+        END,
+        2
+    ) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+    ON s.user_id = c.user_id
+GROUP BY s.user_id;
