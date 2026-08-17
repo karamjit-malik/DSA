6a3d089ccde49dabf8bcf8d5e668f41ad697d1ab
karamjit-malik <135987100+karamjit-malik@users.noreply.github.com>
2026-08-17 19:15:02 +0530

    Time: 1583 ms (26.38%) | Memory: 30.6 MB (19.85%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -2,37 +2,37 @@ class Solution {
 public:
 int n;
 vector<int> dp;
-    int minCut(string s)
-    {
-        n = s.size();
-        if(n<=1)
-        return 0;
-        dp.resize(n,-1);
-        return solve(0,s);
-    }
-    int solve(int i , const string& s)
-    {
-        if(i == n)
-        return 0;
-        if(dp[i]!=-1)
-        return dp[i];
-        int mincost = INT_MAX;
-        string temp = "";
-        for(int j = i ; j<n ; j++)
-        {
-            temp+=s[j];
-            int cost;
-            if(ispalindrome(temp))
-            {
-                if(j == n-1)
-                cost = 0;
-                else
-                cost = 1+solve(j+1,s);
-            }
-            mincost = min(mincost,cost);
-        }
-        return dp[i] = mincost;
-    }
+    // int minCut(string s)
+    // {
+    //     n = s.size();
+    //     if(n<=1)
+    //     return 0;
+    //     dp.resize(n,-1);
+    //     return solve(0,s);
+    // }
+    // int solve(int i , const string& s)
+    // {
+    //     if(i == n)
+    //     return 0;
+    //     if(dp[i]!=-1)
+    //     return dp[i];
+    //     int mincost = INT_MAX;
+    //     string temp = "";
+    //     for(int j = i ; j<n ; j++)
+    //     {
+    //         temp+=s[j];
+    //         int cost;
+    //         if(ispalindrome(temp))
+    //         {
+    //             if(j == n-1)
+    //             cost = 0;
+    //             else
+    //             cost = 1+solve(j+1,s);
+    //         }
+    //         mincost = min(mincost,cost);
+    //     }
+    //     return dp[i] = mincost;
+    // }
     bool ispalindrome(const string& s)
     {
         int m = s.size();
@@ -42,5 +42,32 @@ vector<int> dp;
             return false;
         } 
         return true;
+    }
+    int minCut(string s)
+    {
+        n = s.size();
+        if(n<=1)
+        return 0;
+        dp.resize(n,0);
+        for(int i = n-1 ; i>=0 ; i--)
+        {
+            int mincost = INT_MAX;
+            string temp = "";
+            for(int j = i ; j<n ; j++)
+            {
+                temp+=s[j];
+                int cost;
+                if(ispalindrome(temp))
+                {
+                    if(j == n-1)
+                    cost = 0;
+                    else
+                    cost = 1+dp[j+1];
+                }
+                mincost = min(mincost,cost);
+            }
+            dp[i] = mincost;
+        }
+        return dp[0];
     }
 };
